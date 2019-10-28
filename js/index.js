a2fe51ee4e5a8a406e725a56f955443a6154304d
Jeffery <e_love12134@hotmail.com>
2019-10-28 20:27:47 +0800

    1028-2
    1. 初始化首頁vue
    2. init wowJS

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/* 串`最新消息`api 產生列表 */
+(function() {
+  new Vue({
+    el: ".index_main",
+    data: {
+      list: []
+    },
+    mounted() {},
+    methods: {
+      getList() {}
+    }
+  });
+})();
