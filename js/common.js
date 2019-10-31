4ae86d502fedf23bcb4c737a499da61794d93154
Jeffery <e_love12134@hotmail.com>
2019-10-31 21:37:17 +0800

    1031-1
    新增menu滑動class

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 window.onload = function() {
   new WOW().init();
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > document.querySelector("header").offsetHeight) {
+      document.querySelector("header").classList.add("scroll");
+    } else {
+      document.querySelector("header").classList.remove("scroll");
+    }
+  });
 };
