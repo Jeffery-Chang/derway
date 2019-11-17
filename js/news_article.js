79a576b3dcbef9f33b6e4a2247bcecdb225374de
Jeffery <e_love12134@hotmail.com>
2019-11-17 22:12:21 +0800

    1117-2
    串接最新消息內頁

---
+++ js/news_article.js
@@ -0,0 +1,46 @@
+/* 串`最新消息內頁`api 設置內容 */
+(function() {
+  new Vue({
+    el: ".news_main",
+    data: {
+      article: {
+        title: "",
+        date: "",
+        content: ""
+      }
+    },
+    created() {
+      if (!this.getQueryParams().id) {
+        alert("找不到該則消息！");
+        location.href = "index.html";
+        return;
+      }
+      this.getList();
+    },
+    methods: {
+      getQueryParams(qs) {
+        var paramStr = qs || location.search;
+        paramStr = paramStr.split("+").join(" ");
+        var params = {},
+          tokens,
+          re = /[?&]?([^=]+)=([^&]*)/g;
+        while ((tokens = re.exec(paramStr))) {
+          params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+        }
+        return params;
+      },
+      getList() {
+        axios({
+          method: "get",
+          url: `https://derway.tw/admin/api/news/${this.getQueryParams().id}`
+        })
+          .then(res => {
+            this.article = res.data.data;
+          })
+          .catch(err => {
+            alert("最新消息列表取得失敗！");
+          });
+      }
+    }
+  });
+})();
