8327741ed4d52e6b03ee6ac7c579a6fa1721dd50
Jeffery <e_love12134@hotmail.com>
2019-12-01 15:02:08 +0800

    1201-2
    拿掉console

--- js/news_article.js
+++ js/news_article.js
@@ -36,7 +36,6 @@
         })
           .then(res => {
             this.article = res.data.data;
-            console.log(res.data.data);
           })
           .catch(err => {
             alert("最新消息資料取得失敗！");
